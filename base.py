9a2418c601d674c9ffe08ebede5c6f7817c3a79d
Dump-Line <nodickparty@gmail.com>
2020-06-14 02:59:41 +0300

    random3 commit

--- base.py
+++ base.py
@@ -21,7 +21,7 @@ class Sqlopen:
 		self.cursor.execute(f'INSERT INTO {str(table)} VALUES ({str(text)})')
 		self.connection.commit()	
 	def add_chanell(self, table, chanell):
-		self.cursor.execute(f'INSERT INTO {str(table)} VALUES ({int(chanell)})')
+		self.cursor.execute(f'INSERT INTO {str(table)} VALUES ({str(chanell)})')
 		self.connection.commit()
 	
 		self.connection.commit()
