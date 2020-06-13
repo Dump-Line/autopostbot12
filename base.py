b591e170f207b618930118435c08bcf6bd01683c
Dump-Line <nodickparty@gmail.com>
2020-06-14 01:52:18 +0300

    Fourth commit

--- base.py
+++ base.py
@@ -1,10 +1,21 @@
-import sqlite3
-
+import psycopg2
+DATABASE = DATABASE = {'dbname':"dfmirrdfr3rn0s",
+			'user':"yvjqxthpshyvnq",
+			'password':"48bdc442512a8e66b104e33701aec72627f883fa43fa6573c02a1fe29d492e1b",
+			'host':"ec2-54-247-78-30.eu-west-1.compute.amazonaws.com",
+			'port':"5432" 
+}
 
 class Sqlopen:
 	def __init__(self, base):
-		self.connection = sqlite3.connect(base)
+		self.connection = psycopg2.connect(
+				dbname=DATABASE["dbname"],
+				user=DATABASE["user"],
+				password=DATABASE["password"],
+				host=DATABASE["host"],
+				port=DATABASE["port"])
 		self.cursor = self.connection.cursor()
+
 	
 	def create_table(self, table):
 		self.cursor.execute(f'CREATE TABLE "{str(table)}" (Subject Text, Numbers Text)')
@@ -13,9 +24,6 @@ class Sqlopen:
 	def add_data(self, table, text):
 		self.cursor.execute(f'INSERT INTO "{str(table)}" VALUES ("{str(text)}")')
 		self.connection.commit()	
-#def add_data_text(self, table, text):
-#		self.cursor.execute(f'INSERT INTO "{str(table)}" VALUES ("{str(text)}")')
-
 	def add_chanell(self, table, chanell):
 		self.cursor.execute(f'INSERT INTO "{str(table)}" VALUES ("{str(chanell)}")')
 	
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 from text import *
 import threading
 
+TOKEN = config.token
 bot = telebot.TeleBot(config.token)
 
 
@@ -163,6 +164,21 @@ def send():
 rT = threading.Thread(target = send)
 rT.start()
 
+#######################################
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+  bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+  return "!", 200
 
-if __name__ == '__main__':
-	bot.polling(none_stop=True)
+
+@server.route("/")
+def webhook():
+  bot.remove_webhook()
+  bot.set_webhook(url=HEROKU_LINK + TOKEN)
+  return "!", 200
+
+
+# Получаем новые сообщения
+if __name__ == "__main__":
+  server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000))) 
+  print("START")
