7b758c003a5c3ab84916c24e7baeff1c4ccd26e8
Dump-Line <nodickparty@gmail.com>
2020-06-14 16:13:12 +0300

    final7 commit

--- main.py
+++ main.py
@@ -159,8 +159,7 @@ def send():
 		message_dict = {}
 		for i in base.Sqlopen().returner('chanel'):
 			for x in base.Sqlopen().returner('data'):
-				message = "Заказы АВРОРА КРЫМ" + '\n' + x[0] + '\n' + f'взять заказ Жми ссылку {url}'
-				r = bot.send_message(i[0], message)
+				r = bot.send_message(i[0], f"*Заказы АВРОРА КРЫМ*  \n  {x[0]}  \n  *взять заказ Жми ссылку* {url}", parse_mode= 'Markdown')
 				message_dict[r.message_id] = r.chat.id
 		time.sleep(sleep_time)
 		for i in message_dict.items():
